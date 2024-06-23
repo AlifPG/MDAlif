d87dab9f62f58de37556f650f3efb53350b171ce
MD Alif Islam <160002591+AlifPG@users.noreply.github.com>
2024-06-23 16:51:26 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navber');
+
+
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navber.classList.toggle('active');
+}
